d7e185b2ba92678cbf90362b30dd30400141f315
bbiernacki74 <bbiernacki74@gmail.com>
2017-03-19 18:26:16 +0100

    Poprawki

--- CREATE_ACL.sql
+++ CREATE_ACL.sql
@@ -1,43 +1,48 @@
---SELECT host, lower_port, upper_port, acl
---  FROM dba_network_acls
 
 DECLARE
-    acl_name             VARCHAR2(30) := 'utl_tcp.xml';
-    ftp_server_ip        VARCHAR2(20) := '193.202.117.201';
-    ftp_server_name      VARCHAR2(20) := 'INFINITE_FTP';
-    username             VARCHAR2(30) := 'TETA_ADMIN';
+    acl_name          VARCHAR2 (30) := 'utl_tcp.xml';
+    ftp_server_ip     VARCHAR2 (20) := '193.202.117.201';
+    ftp_server_name   VARCHAR2 (20) := 'INFINITE_FTP';
+    username          VARCHAR2 (30) := 'TETA_ADMIN';
 BEGIN
-    dbms_network_acl_admin.create_acl (
-        acl         => acl_name,
-        description => 'FTP INFINITE Access',
-        principal   => username,
-        is_grant    => TRUE,
-        privilege   => 'connect',
-        start_date  => null,
-        end_date    => null);
-    commit;
+    BEGIN
+        DBMS_NETWORK_ACL_ADMIN.drop_acl (acl => acl_name);
+    EXCEPTION
+        WHEN OTHERS
+        THEN
+            NULL;
+    END;
 
-    dbms_network_acl_admin.add_privilege (
-        acl        => acl_name,
-        principal  => username,
-        is_grant   => TRUE,
-        privilege  => 'connect',
-        start_date => null,
-        end_date   => null);
-    commit;
+    DBMS_NETWORK_ACL_ADMIN.create_acl (acl           => acl_name,
+                                       description   => 'FTP INFINITE Access',
+                                       principal     => username,
+                                       is_grant      => TRUE,
+                                       privilege     => 'connect',
+                                       start_date    => NULL,
+                                       end_date      => NULL);
 
-    dbms_network_acl_admin.assign_acl (
-        acl        => acl_name,
-        host       => ftp_server_name,
-        lower_port => NULL,
-        upper_port => NULL);
-    commit;
-    
-    dbms_network_acl_admin.assign_acl (
-        acl        => acl_name,
-        host       => ftp_server_ip,
-        lower_port => NULL,
-        upper_port => NULL);
-    commit;
+    COMMIT;
+
+
+
+    DBMS_NETWORK_ACL_ADMIN.add_privilege (acl          => acl_name,
+                                          principal    => username,
+                                          is_grant     => TRUE,
+                                          privilege    => 'connect',
+                                          start_date   => NULL,
+                                          end_date     => NULL);
+    COMMIT;
+
+    DBMS_NETWORK_ACL_ADMIN.assign_acl (acl          => acl_name,
+                                       HOST         => ftp_server_name,
+                                       lower_port   => NULL,
+                                       upper_port   => NULL);
+    COMMIT;
+
+    DBMS_NETWORK_ACL_ADMIN.assign_acl (acl          => acl_name,
+                                       HOST         => ftp_server_ip,
+                                       lower_port   => NULL,
+                                       upper_port   => NULL);
+    COMMIT;
 END;
 /
